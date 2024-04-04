0383c8d60e8417709411441d966f03efbf77de80
hriddhisha03 <hriddhisha03@gmail.com>
2024-04-04 08:36:38 +0530

    new changes

--- src/app/Certificates/page.tsx
+++ src/app/Certificates/page.tsx
@@ -5,8 +5,56 @@ type CertificatesProp = {
 
     
 };
-
-const Certificates:React.FC<CertificatesProp> = () => {
+var certificate_count:number = 1;
+const certificates = {
+    1: {
+        'name': 'Certificate 1',
+        'image': '/1.png',
+        'description': "Completion of Postman API fundamentals Student Expert two days workshop by Lets Upgrade and Postman"
+}
+,
+    2: {
+        'name': 'Certificate 2',
+        'image': '/2.png',
+        'description': "INTERNSHIP COMPLETION CERTIFICATE ON DATA SCIENCE (03/2023 - 04/2023) Successfully completed data science live projects from Artifintel in association with Skillvertex"
+}
+,
+    3: {
+        'name': 'Certificate 3',
+        'image': '/3.png',
+        'description': "MICROSOFT AZURE WORKSHOP CERTIFICATE (09/2023) For participating in 'MICROSOFT AZURE WORKSHOP' conducted byRAIT ACM"
+        
+}
+,
+    4: {
+        'name': 'Certificate 4',
+        'image': '/4.png',
+        'description': "TRAINING COMPLETION CERTIFICATE ON DATA SCIENCE (02/2023 - 03/2023)Successfully completed training in data science, held by the SkillVertex company"
+}
+,
+    5: {
+        'name': 'Certificate 5',
+        'image': '/5.png',
+        'description': "RUST PROGRAMMING CERTIFICATE (10/2022) Attended workshop for rust programming , and secured 'A' GRADE organized by college"
+}
+,
+    6: {
+        'name': 'Certificate 6',
+        'image': '/6.png',
+        'description': " Completion certificate for the 'Data Analytics with Specialization in Tableau' workshop offered by Jobaaj Learning!"
+}
+,
+    7: {
+        'name': 'Certificate 7',
+        'image': '/7.png',
+        'description': "Awarded as the best facilitator for the tenure of GDSC 2023-24 AIML and Women In Tech domain"
+}
+}
+const handleDown = () => {
+    certificate_count = (certificate_count + 1)%7;
+    
+}
+    const Certificates:React.FC<CertificatesProp> = () => {
     
     return (
     <>
@@ -23,7 +71,7 @@ const Certificates:React.FC<CertificatesProp> = () => {
     <div className='relative h-3/4 w-1/2 bg-slate-500'>
         
         <button className=" rotate-180 z-50 absolute top-0 ml-96" ><Image src="/arrow2.png" alt="arrow" width={30} height={30} /></button>
-        <Image className='absolute ml-24 top-[53px] ' src="/1.png" alt="arrow" width={600} height={400} />
+        <Image className='absolute ml-24 top-[53px] ' src={certificates[certificate_count]["image"]} alt="arrow" width={600} height={400} />
         <button className="z-50 absolute bottom-0 ml-96" ><Image src="/arrow2.png" alt="arrow" width={30} height={30}/></button>
     </div> 
     </div>
